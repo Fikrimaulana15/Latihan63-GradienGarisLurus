e364290973d2dc237003bf1e9cb544b3b0c654d8
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-10-24 20:20:45 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118074/latihan63/gradiengaris/GarisLurus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan63.gradiengaris;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface GarisLurus {
+    int hitungGradien();
+    
+}
---
+++ src/pbo2/pkg10118074/latihan63/gradiengaris/PBO210118074LATIHAN63GradienGaris.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan63.gradiengaris;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO210118074LATIHAN63GradienGaris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kordinat kordinat = new Kordinat(2,10,5,7);
+        Kordinat kordinat2 = new Kordinat(5,1,3,12);
+        System.out.println(kordinat.hitungGradien());
+        System.out.println(kordinat2.hitungGradien());
+        
+        System.out.println("developed by fikri");
+        
+    }
+    
+}
